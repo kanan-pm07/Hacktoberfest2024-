c8ab2c71da4cad6b0b06230007318bbc1a60d320
Kanan <166215294+kanan-pm07@users.noreply.github.com>
2024-10-09 00:37:57 +0530

    Palindrome.java

---
+++ Java/palindrome.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        // Normalize the string by removing spaces and converting to lowercase
+        String normalizedInput = input.replaceAll("\\s+", "").toLowerCase();
+
+        if (isPalindrome(normalizedInput)) {
+            System.out.println("\"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + input + "\" is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+
+    private static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
